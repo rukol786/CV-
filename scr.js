688c6542280a4f2d4e4e5a72c1a47b578b119d9e
Sana <rukol786786@gmail.com>
2024-07-04 11:41:18 -0700

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,35 @@
+// for(var i=0;i<=10;i++){
+// document.write(i+"Hellowworld "+"<br>");}
+let userCity=prompt("Enter Any City" );
+let cleanestCities=["karachi","lahore","islamabad","Hub"];
+for (let x =0; x < cleanestCities.length; x++){
+   if(userCity==cleanestCities[x]){
+    alert("It is one of the cleanest city");}
+
+ 
+ else{alert("no city found");}
+}
+
+// let firstName=["Muhammad","Faraz"];
+// let lastName=["Abdullah","Haroon"];
+// for(let i=0;i <firstName.length;i++){
+//     for(let j=0; j < lastName.length;j++){
+//     console.log(firstName[i]+""+lastName[j]);
+//     }
+// }
+
+
+// var number=prompt["Enter any number"];
+// for(let i=1;i<10;i++)
+// {
+// document.write(2+"x"+"2"+ "="+2*i+"<br>");
+// }
+// let text= "hellow world";
+// let convert= text. toUpperCase();
+
+// document.write(convert);
+
+// let text= "HELLOW WORLD";
+// let convert= text. toLowerCase();
+
+// document.write(convert);
